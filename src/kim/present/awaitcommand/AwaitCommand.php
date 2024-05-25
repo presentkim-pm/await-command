b15763e036458feda1ff6092c092f29424f07d47
PresentKim <debe3721@gmail.com>
2024-05-25 09:53:22 +0900

    :sparkles: Implements AwaitCommand

---
+++ src/kim/present/awaitcommand/AwaitCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ *
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
+ *
+ * @author       PresentKim (debe3721@gmail.com)
+ * @link         https://github.com/PresentKim
+ * @license      https://opensource.org/licenses/MIT MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ *
+ * @noinspection PhpUnused
+ */
+
+declare(strict_types=1);
+
+namespace kim\present\awaitcomamnd;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use SOFe\AwaitGenerator\Await;
+
+abstract class AwaitCommand extends Command{
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) : void{
+        Await::g2c($this->onExecute($sender, $commandLabel, $args));
+    }
+
+    abstract public function onExecute(CommandSender $sender, string $commandLabel, array $args) : \Generator;
+}
